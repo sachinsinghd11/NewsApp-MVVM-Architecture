d50685e9b95711767f94379b5d26dac004503147
Sachin Singh Dighan <sachinsinghd11@gmail.com>
2025-01-25 13:46:33 +0530

    Update search.

--- app/src/main/java/com/sachin_singh_dighan/newsapp/ui/searchnews/SearchNewsActivity.kt
+++ app/src/main/java/com/sachin_singh_dighan/newsapp/ui/searchnews/SearchNewsActivity.kt
@@ -13,7 +13,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.sachin_singh_dighan.newsapp.NewsApplication
 import com.sachin_singh_dighan.newsapp.data.model.topheadline.Article
 import com.sachin_singh_dighan.newsapp.databinding.ActivitySearchNewsBinding
-import com.sachin_singh_dighan.newsapp.di.component.searchnews.DaggerSearchNewsComponent
 import com.sachin_singh_dighan.newsapp.di.module.searchnews.SearchNewsModule
 import com.sachin_singh_dighan.newsapp.ui.base.BaseActivity
 import com.sachin_singh_dighan.newsapp.ui.common.UiState

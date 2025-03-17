243637aff8dd195ee26f5bfa2f8723aec42963e7
sachinsinghd11 <sachinsinghd11@gmail.com>
2025-03-17 16:55:07 +0530

    Update UiState class.

--- app/src/main/java/com/sachin_singh_dighan/newsapp/ui/common/UiState.kt
+++ app/src/main/java/com/sachin_singh_dighan/newsapp/ui/common/UiState.kt
@@ -2,7 +2,7 @@ package com.sachin_singh_dighan.newsapp.ui.common
 
 sealed interface UiState<out T> {
 
-    data class Success<T>(val data: T?) : UiState<T>
+    data class Success<T>(val data: T) : UiState<T>
 
     data class Error(val message: String) : UiState<Nothing>
 

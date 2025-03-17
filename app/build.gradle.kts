670b42421930fe53b2b6348587dbb51deb6a26ba
sachinsinghd11 <sachinsinghd11@gmail.com>
2025-03-17 16:25:21 +0530

    Add compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("com.google.dagger:hilt-android:2.54")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
     ksp("com.google.dagger:hilt-compiler:2.54")
     implementation("org.glassfish:javax.annotation:10.0-b28")
     implementation("androidx.browser:browser:1.8.0")
@@ -72,13 +74,21 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.compose.runtime:runtime-livedata")
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+    //Navigation
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("androidx.navigation:navigation-compose:2.8.8")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
 
     // UI Tests
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.8.8")
 }

caf46977e37c21d6b6c2a9b4e4b687d0006fcc73
sachinsinghd11 <sachinsinghd11@gmail.com>
2025-03-18 17:55:42 +0530

    Add dispatcherProvider to MainViewModel for testing.

--- app/src/main/java/com/sachin_singh_dighan/newsapp/ui/mainscreen/MainViewModel.kt
+++ app/src/main/java/com/sachin_singh_dighan/newsapp/ui/mainscreen/MainViewModel.kt
@@ -6,18 +6,21 @@ import com.sachin_singh_dighan.newsapp.data.repository.mainscreen.MainRepository
 import com.sachin_singh_dighan.newsapp.ui.base.BaseViewModel
 import com.sachin_singh_dighan.newsapp.ui.common.UiState
 import com.sachin_singh_dighan.newsapp.ui.news.NewsListViewModel
+import com.sachin_singh_dighan.newsapp.utils.DispatcherProvider
 import com.sachin_singh_dighan.newsapp.utils.NetworkHelper
 import com.sachin_singh_dighan.newsapp.utils.logger.Logger
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val mainRepository: MainRepository,
-    private val networkHelper: NetworkHelper,
+    networkHelper: NetworkHelper,
     private val logger: Logger,
+    private val dispatcherProvider: DispatcherProvider,
 ) : BaseViewModel<List<MainSection>>(networkHelper) {
 
     init {
@@ -25,8 +28,9 @@ class MainViewModel @Inject constructor(
     }
 
     private fun getMainSections() {
-        viewModelScope.launch {
+        viewModelScope.launch(dispatcherProvider.main) {
             mainRepository.getDataForMainSection()
+                .flowOn(dispatcherProvider.default)
                 .catch { e ->
                     _uiState.value = UiState.Error(e.toString())
                     logger.d(NewsListViewModel.TAG, e.toString())

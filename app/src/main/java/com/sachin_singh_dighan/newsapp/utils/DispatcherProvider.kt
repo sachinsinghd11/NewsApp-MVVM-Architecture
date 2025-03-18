d8cce4a1f2ee33301a84a7a87ae4d53da12db21f
sachinsinghd11 <sachinsinghd11@gmail.com>
2025-03-18 15:19:33 +0530

    Modify DefaultDispatcherProvider.

--- app/src/main/java/com/sachin_singh_dighan/newsapp/utils/DispatcherProvider.kt
+++ app/src/main/java/com/sachin_singh_dighan/newsapp/utils/DispatcherProvider.kt
@@ -4,13 +4,13 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 interface DispatcherProvider {
-    fun main() : CoroutineDispatcher
-    fun io() : CoroutineDispatcher
-    fun default() : CoroutineDispatcher
+    val main: CoroutineDispatcher
+    val io: CoroutineDispatcher
+    val default: CoroutineDispatcher
 }
 
 class DefaultDispatcherProvider : DispatcherProvider {
-    override fun main(): CoroutineDispatcher = Dispatchers.Main
-    override fun io(): CoroutineDispatcher = Dispatchers.IO
-    override fun default(): CoroutineDispatcher = Dispatchers.Default
+    override val main: CoroutineDispatcher = Dispatchers.Main
+    override val io: CoroutineDispatcher = Dispatchers.IO
+    override val default: CoroutineDispatcher = Dispatchers.Default
 }

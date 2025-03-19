a86cbf6becde2290fc1df968acc747237b9bc58d
sachinsinghd11 <sachinsinghd11@gmail.com>
2025-03-19 12:46:54 +0530

    Update TopHeadLineViewModel for test case.

--- app/src/main/java/com/sachin_singh_dighan/newsapp/ui/topheadline/TopHeadLineViewModel.kt
+++ app/src/main/java/com/sachin_singh_dighan/newsapp/ui/topheadline/TopHeadLineViewModel.kt
@@ -6,12 +6,11 @@ import com.sachin_singh_dighan.newsapp.data.model.topheadline.Article
 import com.sachin_singh_dighan.newsapp.data.repository.topheadline.TopHeadLineRepository
 import com.sachin_singh_dighan.newsapp.ui.base.BaseViewModel
 import com.sachin_singh_dighan.newsapp.ui.common.UiState
+import com.sachin_singh_dighan.newsapp.utils.DispatcherProvider
 import com.sachin_singh_dighan.newsapp.utils.NetworkHelper
 import com.sachin_singh_dighan.newsapp.utils.logger.Logger
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
@@ -23,30 +22,23 @@ class TopHeadLineViewModel @Inject constructor(
     private val topHeadLineRepository: TopHeadLineRepository,
     private val networkHelper: NetworkHelper,
     private val logger: Logger,
+    private val dispatcherProvider: DispatcherProvider,
 ) : BaseViewModel<List<Article>>(networkHelper) {
 
     companion object {
         const val TAG = "TopHeadLineViewModel"
     }
 
-    /*// Track if an error dialog is showing
-    private val _showErrorDialog = MutableStateFlow(false)
-    val showErrorDialog: StateFlow<Boolean> = _showErrorDialog
-
-    // Store error message
-    private val _errorMessage = MutableStateFlow("")
-    val errorMessage: StateFlow<String> = _errorMessage*/
-
     init {
         fetchTopHeadlines()
     }
 
     private fun fetchTopHeadlines() {
-        viewModelScope.launch {
+        viewModelScope.launch(dispatcherProvider.main) {
             _uiState.value = UiState.Loading
             if (networkHelper.isNetworkAvailable()) {
                 topHeadLineRepository.getTopHeadLinesByDefault()
-                    .flowOn(Dispatchers.IO)
+                    .flowOn(dispatcherProvider.io)
                     .catch { e ->
                         handleError(e)
                         logger.d(TAG, e.toString())

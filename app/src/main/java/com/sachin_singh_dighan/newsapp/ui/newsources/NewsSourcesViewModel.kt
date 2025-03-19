a1940704db3f2142fbc42a5a7eb394aac69c0564
sachinsinghd11 <sachinsinghd11@gmail.com>
2025-03-19 14:42:49 +0530

    Update NewsSourcesViewModel for test cases.

--- app/src/main/java/com/sachin_singh_dighan/newsapp/ui/newsources/NewsSourcesViewModel.kt
+++ app/src/main/java/com/sachin_singh_dighan/newsapp/ui/newsources/NewsSourcesViewModel.kt
@@ -6,10 +6,10 @@ import com.sachin_singh_dighan.newsapp.data.model.newsources.Sources
 import com.sachin_singh_dighan.newsapp.data.repository.newsources.NewSourcesRepository
 import com.sachin_singh_dighan.newsapp.ui.base.BaseViewModel
 import com.sachin_singh_dighan.newsapp.ui.common.UiState
+import com.sachin_singh_dighan.newsapp.utils.DispatcherProvider
 import com.sachin_singh_dighan.newsapp.utils.NetworkHelper
 import com.sachin_singh_dighan.newsapp.utils.logger.Logger
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
@@ -21,6 +21,7 @@ class NewsSourcesViewModel @Inject constructor(
     private val newSourcesRepository: NewSourcesRepository,
     private val networkHelper: NetworkHelper,
     private val logger: Logger,
+    private val dispatcherProvider: DispatcherProvider,
 ) : BaseViewModel<List<Sources>>(networkHelper) {
 
     companion object {
@@ -32,10 +33,10 @@ class NewsSourcesViewModel @Inject constructor(
     }
 
     private fun fetchNewSource() {
-        viewModelScope.launch {
+        viewModelScope.launch(dispatcherProvider.main) {
             if (networkHelper.isNetworkAvailable()) {
                 newSourcesRepository.getNewResources()
-                    .flowOn(Dispatchers.IO)
+                    .flowOn(dispatcherProvider.io)
                     .catch { e ->
                         handleError(e)
                         _uiState.value = UiState.Error(e.toString())

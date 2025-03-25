9597c26c18fb00f12603eb9e086ee80e217111e8
sachinsinghd11 <sachinsinghd11@gmail.com>
2025-03-25 19:25:20 +0530

    Add toSourceEntity() as extension to ApiSource.

--- app/src/main/java/com/sachin_singh_dighan/newsapp/data/model/topheadline/ApiSource.kt
+++ app/src/main/java/com/sachin_singh_dighan/newsapp/data/model/topheadline/ApiSource.kt
@@ -1,10 +1,18 @@
 package com.sachin_singh_dighan.newsapp.data.model.topheadline
 
 import com.google.gson.annotations.SerializedName
+import com.sachin_singh_dighan.newsapp.data.local.entity.Source
 
-data class Source(
+data class ApiSource(
     @SerializedName("id")
     val id: String? = null,
     @SerializedName("name")
     val name: String = "",
 )
+
+fun ApiSource.toSourceEntity(): Source {
+    return Source(
+        id = this.id,
+        name = this.name
+    )
+}

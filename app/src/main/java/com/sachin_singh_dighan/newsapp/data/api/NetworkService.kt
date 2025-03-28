57ce1ecfc7354667e7e74cd9f096c34bb41a4a35
sachinsinghd11 <sachinsinghd11@gmail.com>
2025-03-28 18:37:57 +0530

    Add paging network service method.

--- app/src/main/java/com/sachin_singh_dighan/newsapp/data/api/NetworkService.kt
+++ app/src/main/java/com/sachin_singh_dighan/newsapp/data/api/NetworkService.kt
@@ -2,7 +2,6 @@ package com.sachin_singh_dighan.newsapp.data.api
 
 import com.sachin_singh_dighan.newsapp.data.model.newsources.NewSourceResponse
 import com.sachin_singh_dighan.newsapp.data.model.topheadline.TopHeadLinesResponse
-import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 import retrofit2.http.Query
 import javax.inject.Singleton
@@ -15,6 +14,13 @@ interface NetworkService {
 
     @GET("top-headlines")
     suspend fun getTopHeadLinesByCountry(@Query("country") country: String): TopHeadLinesResponse
+
+    @GET("top-headlines")
+    suspend fun getTopHeadLinesPaginationByCountry(
+        @Query("country") country: String,
+        @Query("page") page: Int,
+        @Query("pageSize") pageSize: Int,
+    ): TopHeadLinesResponse
 
     @GET("top-headlines")
     suspend fun getTopHeadLinesByLanguage(@Query("language") language: String): TopHeadLinesResponse
